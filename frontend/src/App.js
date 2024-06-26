74654b5aaf321249cd2a6981094ec94e61c2141e
Rumanda Senanayake <rumandasenanayake@gmail.com>
2024-06-26 23:45:53 +0530

    Hero Section

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Hero from './Components/Hero/Hero'
+import About from './Components/About/About';
+
+function App() {
+  return (
+    <div className="App">
+      <Hero/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/Hero/Hero.jsx
@@ -0,0 +1,31 @@
+// src/components/Hero/Hero.jsx
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <div className="hero">
+
+<div className="hero-section">
+      <h2> EXPLORE THE REAL</h2>
+      <h1>WILD LIFE</h1>
+      <p>Welcome to WildLife Wonders, your gateway to the fascinating world of wildlife.</p>
+    </div>
+
+    <div className="about-container">
+        
+      <h2>ABOUT WILD LIFE</h2>
+      <p>At WildLife Wonders, our mission is to educate and inspire people to protect and preserve the rich biodiversity of our planet. We believe that understanding and appreciating the wonders of wildlife is the first step towards ensuring their survival for future generations. Our goal is to create a platform where people can learn about various species, their habitats, and the pressing conservation issues they face.</p>
+
+<p>WildLife Wonders was founded by a group of passionate wildlife enthusiasts and conservationists who recognized the urgent need to raise awareness about the plight of endangered species and the critical importance of preserving natural habitats. What began as a small initiative has grown into a comprehensive resource for wildlife education and conservation advocacy, reaching audiences around the world.</p>
+
+<p>We believe in a multi-faceted approach to wildlife conservation that includes education, advocacy, and direct action. Through our website, we provide detailed information about various wildlife species and their habitats, highlight major conservation efforts, and offer practical ways for individuals to get involved. We also collaborate with other conservation organizations and support on-the-ground projects that have a tangible impact on wildlife protection..</p>
+    </div>
+
+    </div>
+    
+    
+  );
+};
+
+export default Hero;
