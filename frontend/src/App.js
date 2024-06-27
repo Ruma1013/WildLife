310e40a1b717c2dfdb31c49bb08a5cf28763c19c
Rumanda Senanayake <rumandasenanayake@gmail.com>
2024-06-27 21:48:00 +0530

    DiscoverUs Section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Hero from './Components/Hero/Hero'
+import Hero from './Components/Hero/Hero';
+import Discover from './Components/Discover/Discover';
 
 function App() {
   return (
     <div className="App">
       <Hero/>
+      <Discover/>
       
     </div>
   );
